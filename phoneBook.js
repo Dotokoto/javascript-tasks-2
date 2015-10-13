5f0b3a7b2455059b329a4e243a787689e6d93c98
Natalia Aksenova <dotokoto@gmail.com>
2015-10-13 18:10:58 +0500

    update

--- phoneBook.js
+++ phoneBook.js
@@ -4,162 +4,161 @@ var phoneBook = []; // Здесь вы храните записи как хот
 var MAX_NAME_LENGTH = 50;
 var MAX_PHONE_LENGTH = 22;
 var MAX_EMAIL_LENGTH = 40;
+var PHONE_REGEXP = /^\+?\d{0,3}?\s*(?:\(\d{3}\)|\d{3})\s*\d{3}(?:-|\s+)?\d(?:-|\s+)?\d{3}$/;
+var EMAIL_REGEXP = /^[^\.@]+@[^\.@]+\.[^@]+$/;
+var PHONE_FIND_REGEXP = /(?:[-+0-9()])+/;
 
 /*
  Функция добавления записи в телефонную книгу.
  На вход может прийти что угодно, будьте осторожны.
  */
-module.exports.add = function add(name, phone, email) {
-    if (name === null || !checkPhone(phone) || !checkEmail(email)) {
+function add(name, phone, email) {
+    if (name === null || !PHONE_REGEXP.test(phone) || !EMAIL_REGEXP.test(email)) {
         return;
     }
-    phone = phone.replace(/\s|\(|\)|\+|-/g, '');
-    name = name.replace(/\r\n|\r|\n/g, '');
-    email = email.replace(/\r\n|\r|\n/g, '');
-    phoneBook.push(
-        {
-            name: name,
-            phone: phone,
-            email: email
-        }
-    );
-};
-
-function checkPhone(phone) {
-    var phoneRegexp = /^\+?\d{0,3}?\s*(?:\(\d{3}\)|\d{3})\s*\d{3}(?:-|\s+)?\d(?:-|\s+)?\d{3}$/;
-    return phoneRegexp.test(phone);
-}
-
-function checkEmail(email) {
-    var emailRegexp = /^[^\.@]+@[^\.@]+\.[^@]+$/;
-    return emailRegexp.test(email);
+    phone = normalizePhone(phone);
+    name = normalizeString(name);
+    email = normalizeString(email);
+    phoneBook.push(createRecord(name, phone, email));
 }
 
 /*
  Функция поиска записи в телефонную книгу.
  Поиск ведется по всем полям.
  */
-module.exports.find = function find(query) {
+function find(query) {
     var recordFound = [];
-    if (typeof (query) === 'undefined') {
-        query = '';
-    }
-    var phoneReg = /(?:[-+0-9()])+/;
-    if (phoneReg.test(query)) {
-        query = query.replace(/\s|\(|\)|\+|-/g, '');
+    query = query || '';
+    if (PHONE_FIND_REGEXP.test(query)) {
+        query = normalizePhone(query);
     }
     for (var i = 0; i < phoneBook.length; i++) {
-        if (phoneBook[i].name.indexOf(query) > -1 || phoneBook[i].phone.indexOf(query) > -1 ||
-            phoneBook[i].email.indexOf(query) > -1) {
+        if (findInRecord(phoneBook[i], query)) {
             recordFound.push(
-                {
-                    name: phoneBook[i].name,
-                    phone: phoneBook[i].phone,
-                    email: phoneBook[i].email
-                }
+                createRecord(phoneBook[i].name, phoneBook[i].phone, phoneBook[i].email)
             );
         }
     }
     return recordFound;
-};
+}
 
-module.exports.printRecords = function printRecords(records) {
+function findInRecord(record, query) {
+    return (record.name.indexOf(query) !== -1 || record.phone.indexOf(query) !== -1 ||
+    record.email.indexOf(query) !== -1);
+}
+
+function normalizePhone(phone) {
+    return phone.replace(/\s|\(|\)|\+|-/g, '');
+}
+
+function normalizeString(string) {
+    return string.replace(/\r\n|\r|\n/g, '');
+}
+
+function createRecord(name, phone, email) {
+    return {name: name, phone: phone, email: email};
+}
+
+function printRecords(records) {
     for (var i = 0; i < records.length; i++) {
         console.log(records[i].name + ', ' +
-            module.exports.phoneToPrint(records[i].phone) + ', ' + records[i].email);
+            phoneToPrint(records[i].phone) + ', ' + records[i].email);
     }
-};
+}
 
 /*
  Функция удаления записи в телефонной книге.
  */
-module.exports.remove = function remove(query) {
-    if (typeof (query) === 'undefined') {
-        query = '';
+function remove(query) {
+    query = query || '';
+    if (PHONE_FIND_REGEXP.test(query)) {
+        query = normalizePhone(query);
     }
-    var phoneReg = /(?:[-+0-9()])+/;
-    if (phoneReg.test(query)) {
-        query = query.replace(/\s|\(|\)|\+|-/g, '');
-    }
-    var deletedRecords = 0;
+    var deletedRecords = [];
     for (var i = 0; i < phoneBook.length; i++) {
-        if (phoneBook[i].name.indexOf(query) > -1 || phoneBook[i].phone.indexOf(query) > -1 ||
-            phoneBook[i].email.indexOf(query) > -1) {
+        if (findInRecord(phoneBook[i], query)) {
             phoneBook.splice(i, 1);
             i--;
-            deletedRecords++;
+            deletedRecords.push(
+                createRecord(phoneBook[i].name, phoneBook[i].phone, phoneBook[i].email)
+            );
         }
     }
-    console.log('Удалено записей: ' + deletedRecords);
-};
+    return deletedRecords;
+}
 
 /*
  Функция импорта записей из файла (задача со звёздочкой!).
  */
-module.exports.importFromCsv = function importFromCsv(filename) {
+function importFromCsv(filename) {
     var data = require('fs').readFileSync(filename, 'utf-8');
-    var fileRecords = data.split(/\n/);
-    var record;
-    for (var i = 0; i < fileRecords.length; i++) {
-        record = fileRecords[i].split(';');
-        module.exports.add(record[0], record[1], record[2]);
+    var fileRecords = data.split('\n');
+    if (fileRecords.length > 0 && fileRecords[0].split(';').length === 3) {
+        var record;
+        for (var i = 0; i < fileRecords.length; i++) {
+            record = fileRecords[i].split(';');
+            add(record[0], record[1], record[2]);
+        }
     }
-};
+}
 
 /*
  Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
  */
-module.exports.showTable = function showTable() {
-    var records = module.exports.find('');
+function showTable() {
+    var records = find('');
     // 2 - пробел слева и справа, 3 - пробел-палка-пробел
     var lineLength = MAX_NAME_LENGTH + 3 + MAX_PHONE_LENGTH + 3 + MAX_EMAIL_LENGTH + 2;
     var string = '';
-    string += '╒' + module.exports.repeatSymbols('═', MAX_NAME_LENGTH + 2) + '╤' +
-        module.exports.repeatSymbols('═', MAX_PHONE_LENGTH + 2) + '╤' +
-        module.exports.repeatSymbols('═', MAX_EMAIL_LENGTH + 2) + '╕' + '\n';
+    string += '╒' + repeatSymbols('═', MAX_NAME_LENGTH + 2) + '╤' +
+        repeatSymbols('═', MAX_PHONE_LENGTH + 2) + '╤' +
+        repeatSymbols('═', MAX_EMAIL_LENGTH + 2) + '╕' + '\n';
     string += '│ Имя';
     //3, 7, 5 - длина текста шапки
-    string += module.exports.repeatSymbols(' ', MAX_NAME_LENGTH - 3 + 1);
+    string += repeatSymbols(' ', MAX_NAME_LENGTH - 3 + 1);
     string += '│ Телефон';
-    string += module.exports.repeatSymbols(' ', MAX_PHONE_LENGTH - 7 + 1);
+    string += repeatSymbols(' ', MAX_PHONE_LENGTH - 7 + 1);
     string += '│ Email';
-    string += module.exports.repeatSymbols(' ', MAX_EMAIL_LENGTH - 5 + 1);
+    string += repeatSymbols(' ', MAX_EMAIL_LENGTH - 5 + 1);
     string += '│\n├';
-    string += module.exports.repeatSymbols('─', lineLength);
+    string += repeatSymbols('─', lineLength);
     string += '┤\n';
-    var name;
-    var phone;
-    var email;
     for (var i = 0; i < records.length; i++) {
-        name = records[i].name;
-        phone = module.exports.phoneToPrint(records[i].phone);
-        email = records[i].email;
+        var name = records[i].name;
+        var phone = phoneToPrint(records[i].phone);
+        var email = records[i].email;
         string +=
-            '│ ' + name + module.exports.repeatSymbols(' ', MAX_NAME_LENGTH - name.length + 1) +
-             '│ ' + phone +
-            module.exports.repeatSymbols(' ', MAX_PHONE_LENGTH - phone.length + 1) +
-             '│ ' + email +
-            module.exports.repeatSymbols(' ', MAX_EMAIL_LENGTH - email.length + 1) +
-             '│' + '\n';
+            '│ ' + name + repeatSymbols(' ', MAX_NAME_LENGTH - name.length + 1) + '│ ' + phone +
+            repeatSymbols(' ', MAX_PHONE_LENGTH - phone.length + 1) + '│ ' + email +
+            repeatSymbols(' ', MAX_EMAIL_LENGTH - email.length + 1) + '│' + '\n';
     }
-    string += '╘' + module.exports.repeatSymbols('═', MAX_NAME_LENGTH + 2) + '╧' +
-        module.exports.repeatSymbols('═', MAX_PHONE_LENGTH + 2) + '╧' +
-        module.exports.repeatSymbols('═', MAX_EMAIL_LENGTH + 2) + '╛' + '\n';
+    string += '╘' + repeatSymbols('═', MAX_NAME_LENGTH + 2) + '╧' +
+        repeatSymbols('═', MAX_PHONE_LENGTH + 2) + '╧' +
+        repeatSymbols('═', MAX_EMAIL_LENGTH + 2) + '╛' + '\n';
     console.log(string);
-};
+}
 
-module.exports.repeatSymbols = function repeatSymbols(sym, count) {
+function repeatSymbols(sym, count) {
     var string = '';
     for (var i = 0; i < count; i++) {
         string += sym;
     }
     return string;
-};
+}
 
-module.exports.phoneToPrint = function phoneToPrint(phone) {
+function phoneToPrint(phone) {
     if (phone.length >= 11) {
         phone = '+' + phone;
     }
     phone = phone.replace(/^([+0-9]*)(\d{3})(\d{3})(\d)(\d{3})$/, '$1 ($2) $3-$4-$5');
     return phone;
+}
+
+module.exports = {
+    add: add,
+    find: find,
+    printRecords: printRecords,
+    remove: remove,
+    importFromCsv: importFromCsv,
+    showTable: showTable
 };
